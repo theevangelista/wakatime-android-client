ef8a9a38fe8fa32511bdfdc7cbedba7a76d1dbdf
João Pedro Evangelista <joaoevangelista@protonmail.com>
2016-08-09 16:05:55 -0300

    Try to echo license into android home

---
+++ add-licenses.sh
@@ -0,0 +1,5 @@
+echo "Android Home: $ANDROID_HOME"
+mkdir -p "$ANDROID_HOME/licenses"
+touch "$ANDROID_HOME/licenses/android-sdk-preview-license"
+echo -e "\n504667f4c0de7af1a06de9f4b1727b84351f2910" > "$ANDROID_HOME/licenses/android-sdk-preview-license"
+echo -e "8933bad161af4178b1185d1a37fbf41ea5269c55" > "$ANDROID_HOME/licenses/android-sdk-license"
