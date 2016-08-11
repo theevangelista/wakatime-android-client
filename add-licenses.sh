211a9088c09e59a6e12ca8d12d672532ae97d86f
João Pedro Evangelista <joaoevangelista@protonmail.com>
2016-08-11 15:20:36 -0300

    Touch missing license file

--- add-licenses.sh
+++ add-licenses.sh
@@ -1,5 +1,6 @@
 echo "Android Home: $ANDROID_HOME"
 mkdir -p "$ANDROID_HOME/license"
 touch "$ANDROID_HOME/license/android-sdk-preview-license"
+touch "$ANDROID_HOME/license/android-sdk-license"
 echo -e "\n504667f4c0de7af1a06de9f4b1727b84351f2910" > "$ANDROID_HOME/license/android-sdk-preview-license"
 echo -e "8933bad161af4178b1185d1a37fbf41ea5269c55" > "$ANDROID_HOME/license/android-sdk-license"
